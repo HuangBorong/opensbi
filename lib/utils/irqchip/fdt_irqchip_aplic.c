864f8cd771fca10b3689082047df236b5b7a8e4e
Huang Borong <huangborong@bosc.ac.cn>
2025-01-01 14:36:28 +0800

    feat(irqchip): add debug dumping function for APLIC data

--- lib/utils/irqchip/fdt_irqchip_aplic.c
+++ lib/utils/irqchip/fdt_irqchip_aplic.c
@@ -15,6 +15,28 @@
 #include <sbi_utils/fdt/fdt_helper.h>
 #include <sbi_utils/irqchip/fdt_irqchip.h>
 #include <sbi_utils/irqchip/aplic.h>
+#include <sbi/sbi_console.h>
+
+static void dump_aplic_data(struct aplic_data *aplic)
+{
+	sbi_printf("APLIC %s-mode interrupt domain at: 0x%lx, size: 0x%lx\n", 
+		aplic->targets_mmode ? "M" : "S", aplic->addr, aplic->size);
+	sbi_printf("num IRQ sources: %lu\n" , aplic->num_source);
+	sbi_printf("num IDCs: %lu\n" , aplic->num_idc);
+	sbi_printf("MMSIADDRCFG: %s\n", aplic->has_msicfg_mmode ? "true" : "false");
+	sbi_printf("SMSIADDRCFG: %s\n", aplic->has_msicfg_smode ? "true" : "false");
+
+	for (int i = 0; i < APLIC_MAX_DELEGATE; i++)
+	{
+		if (aplic->delegate[i].first_irq || aplic->delegate[i].last_irq || aplic->delegate[i].child_index)
+		{
+			sbi_printf("delegate[%d]: child_index: %u, first_irq: %u, last_irq: %u\n", 
+				i, aplic->delegate[i].child_index, aplic->delegate[i].first_irq, aplic->delegate[i].last_irq);
+		}		
+	}
+
+	sbi_printf("\n");
+}
 
 static int irqchip_aplic_cold_init(const void *fdt, int nodeoff,
 				   const struct fdt_match *match)
@@ -29,6 +51,8 @@ static int irqchip_aplic_cold_init(const void *fdt, int nodeoff,
 	rc = fdt_parse_aplic_node(fdt, nodeoff, pd);
 	if (rc)
 		goto fail_free_data;
+
+	dump_aplic_data(pd);
 
 	rc = aplic_cold_irqchip_init(pd);
 	if (rc)
